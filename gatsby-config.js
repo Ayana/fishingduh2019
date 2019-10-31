d75245c8a0ec550ae110a946e3afb11966405c56
Ayana Osawa <>
2019-10-31 17:43:19 -0400

    Remove old instagram plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,19 +28,12 @@ module.exports = {
       },
     },
     {
-      // Plugin: gatsby-source-instagram-all - Instagram.js
-      resolve: `gatsby-source-instagram-all`,
-      options: {
-        access_token: "2073134132.d1bad15.faf29b1f4d204935a64b22416267da36"
-      }
-    },
-    {
       // Plugin: gatsby-source-instagram - Insta.js
       resolve: `gatsby-source-instagram`,
       options: {
-        username: `misawayanohanashi`,
-        access_token: "2073134132.d1bad15.faf29b1f4d204935a64b22416267da36",
-        instagram_id: "misawayanohanashi",
+        username: `fishingduh`,
+        // access_token: "2073134132.d1bad15.faf29b1f4d204935a64b22416267da36",
+        // instagram_id: "misawayanohanashi",
       },
     },  
     `gatsby-plugin-sass`,
